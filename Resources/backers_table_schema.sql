ce25f27eb0da5740022be87f611dfb214490169e
David Bowen <davidbowen1776@gmail.com>
2022-12-29 11:55:27 -0500

    Adding Resources file

---
+++ Resources/backers_table_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
